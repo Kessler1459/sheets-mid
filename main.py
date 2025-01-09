068879cd49cf7c7c9b75ce7b5ab2e67c445b94ae
Ivan Libonatti <kessler1459@gmail.com>
2025-01-08 23:07:01 -0300

    binance api

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
 
 from data.gsheet import GSheet
+from scraping.index.binance import Binance
 
-# The ID and range of a sample spreadsheet.
+
 SPREADSHEET_ID = os.environ['SPREADSHEET']
 SPREADSHEET_PAGE = os.environ['SPREADSHEET_PAGE']
+BINANCE_API_KEY = os.environ['BINANCE_API_KEY']
+BINANCE_SECRET_KEY = os.environ['BINANCE_SECRET_KEY']
+BINANCE_ROOT_URL= os.environ['BINANCE_ROOT_URL']
+
+binance = Binance(['ETHUSDT'], BINANCE_ROOT_URL, BINANCE_API_KEY)
+res = binance.update_values()
 
 gsheet = GSheet(SPREADSHEET_ID, SPREADSHEET_PAGE)
 gsheet.worksheet.append_row(['1', '2', '3'])
