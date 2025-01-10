910e2528bda00f972b58a9f9e65d7a7ed01650ac
Ivan Libonatti <kessler1459@gmail.com>
2025-01-10 00:33:33 -0300

    abstract provider

---
+++ scraping/news/news_provider.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from typing import Iterable
+
+
+class NewsProvider(ABC):
+    news: dict[str, str]
+    
+    @abstractmethod
+    def __init__(self, searchs: Iterable, *args):
+        pass
+
+    @abstractmethod
+    def update_values(self) -> dict[str, str]:
+        pass
